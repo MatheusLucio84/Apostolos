6f3ae4e558790ccf7bd3d1718d5bbe332b21989b
MatheusLucio84 <bhmatheus@gmail.com>
2024-09-07 12:05:16 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ function pesquisar() {
     // Obtém a seção onde os resultados da pesquisa serão exibidos
     let section = document.getElementById("resultados-pesquisa");
  
-    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
    
+    //se campoPesquisa para string sm nada
     if (!campoPesquisa) {
-        section.innerHTML = "NADA FOI ENCONTRADO, DIGITE SUA PESQUISA"
+        section.innerHTML = "<h2><p>NADA FOI ENCONTRADO <br> DIGITE ALGO PARA SUA PESQUISA</p></h2>"
         return
     }
     campoPesquisa = campoPesquisa.toLowerCase()
@@ -14,11 +15,12 @@ function pesquisar() {
     let titulo = "";
     let descrição = "";
     let tags = "";
-    // Itera sobre cada dado na lista de dados
+    // Intera sobre cada dado na lista de dados
     for (let dado of dados) {
         titulo = dado.titulo.toLocaleLowerCase()
         descrição = dado.descrição.toLocaleLowerCase()
         tags = dado.tags.toLocaleLowerCase()
+        //se título inclui campoPesquisa
     if(titulo.includes(campoPesquisa) || descrição.includes(campoPesquisa) || tags.includes(campoPesquisa))
         {
 //cria novo elemento
@@ -29,14 +31,16 @@ function pesquisar() {
                 <h2>
                     <a href="#" target="_blank">${dado.titulo}</a> </h2>
                 <p class="Descricao-meta">${dado.descrição}</p> 
-                <a href=${dado.link} target="_blank">Mais informações</a> </div>
+                <a href=${dado.link} target="_blank">Mais informações</a> 
+                </div>
         `;
     }
 }
 
 if(!resultados){
-    resultados = <p>"NADA FOI ENCONTRADO"</p>;
-}
+    resultados = "<p><h2>NADA FOI ENCONTRADO</h2></p>";
+    }
+    
     // Atribui o HTML gerado à seção de resultados
     section.innerHTML = resultados;
 }
